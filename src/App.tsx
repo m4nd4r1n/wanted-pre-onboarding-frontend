bdcf3b01a61aeaf8fb6757a96c0467f354bd59a1
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-06-10 01:49:01 +0900

    feat: 로그인, 회원가입 route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <></>;
-}
+import { PATHNAMES } from './constants/pathnames';
+import AuthPage from './pages/AuthPage';
+import { Route, Routes } from 'react-router-dom';
+
+const { SIGN_IN, SIGN_UP } = PATHNAMES;
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path={SIGN_UP} element={<AuthPage type={SIGN_UP} />}></Route>
+      <Route path={SIGN_IN} element={<AuthPage type={SIGN_IN} />}></Route>
+    </Routes>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import App from './App';
 import './index.css';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );

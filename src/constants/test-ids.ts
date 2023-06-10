1fcafcf0f40048da8271e7c7889ee93effa62964
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-06-11 00:09:26 +0900

    feat: Todo test id 작성

--- src/constants/test-ids.ts
+++ src/constants/test-ids.ts
@@ -3,4 +3,11 @@ export const TEST_IDS = {
   PASSWORD_INPUT: 'password-input',
   SIGNIN_BUTTON: 'signin-button',
   SIGNUP_BUTTON: 'signup-button',
+  NEW_TODO_INPUT: 'new-todo-input',
+  NEW_TODO_ADD_BUTTON: 'new-todo-add-button',
+  MODIFY_BUTTON: 'modify-button',
+  DELETE_BUTTON: 'delete-button',
+  MODIFY_INPUT: 'modify-input',
+  SUBMIT_BUTTON: 'submit-button',
+  CANCEL_BUTTON: 'cancel-button',
 } as const;

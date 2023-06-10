4206aeed3aa520ec8d03d2f11fea1b4231970513
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-06-10 18:40:22 +0900

    feat: 로그인 여부에 따른 리다이렉트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import { PATHNAMES } from './constants/pathnames';
 import AuthPage from './pages/AuthPage';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
-const { SIGN_IN, SIGN_UP } = PATHNAMES;
+const { SIGN_IN, SIGN_UP, TODO } = PATHNAMES;
+
+const AuthRoute = () => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    return <Navigate to={TODO} />;
+  }
+  return <Outlet />;
+};
+const PrivateRoute = () => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return <Navigate to={SIGN_IN} />;
+  }
+  return <Outlet />;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path={SIGN_UP} element={<AuthPage type={SIGN_UP} />}></Route>
-      <Route path={SIGN_IN} element={<AuthPage type={SIGN_IN} />}></Route>
+      <Route path='/' element={<Navigate to={TODO} />}></Route>
+      <Route element={<AuthRoute />}>
+        <Route path={SIGN_UP} element={<AuthPage type={SIGN_UP} />}></Route>
+        <Route path={SIGN_IN} element={<AuthPage type={SIGN_IN} />}></Route>
+      </Route>
+      <Route element={<PrivateRoute />}>
+        <Route path={TODO} element={<></>}></Route>
+      </Route>
     </Routes>
   );
 };

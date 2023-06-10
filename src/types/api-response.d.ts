f0018015ac27e5f1ff75c9ed071294dfb450df0c
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-06-10 21:25:46 +0900

    feat: Todo 타입 및 API 응답 타입 작성

--- src/types/api-response.d.ts
+++ src/types/api-response.d.ts
@@ -1,5 +1,11 @@
 declare module 'api-response' {
+  import type { Todo } from '@/types/todo';
+
   export type SignInResponse = {
     access_token: string;
   };
+
+  export type CreateTodoResponse = Todo;
+  export type GetTodosResponse = Todo[];
+  export type UpdateTodoResponse = Todo;
 }

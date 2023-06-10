f7b6a8b2ac045e73b934fd9a869b7a7c388b2964
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-06-11 01:12:30 +0900

    feat: 로그아웃 구현

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,5 +1,8 @@
-import { HiOutlineChevronLeft } from 'react-icons/hi';
+import { HiArrowRightOnRectangle, HiOutlineChevronLeft } from 'react-icons/hi2';
 import { useNavigate } from 'react-router-dom';
+
+import { PATHNAMES } from '@/constants/pathnames';
+import { STORAGE_KEY } from '@/constants/storage-key';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -7,11 +10,22 @@ interface LayoutProps {
   hasBackButton?: boolean;
 }
 
+const { SIGN_IN } = PATHNAMES;
+const { TODOS, TOKEN } = STORAGE_KEY;
+const HEADER_ICON_SIZE = 24;
+
 const Layout: React.FC<LayoutProps> = ({ children, title, hasBackButton }) => {
   const navigate = useNavigate();
 
+  const token = localStorage.getItem(TOKEN);
+
   const onBackClick: React.MouseEventHandler<HTMLButtonElement> = () => {
     navigate(-1);
+  };
+  const onLogoutClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+    localStorage.removeItem(TOKEN);
+    localStorage.removeItem(TODOS);
+    navigate(SIGN_IN);
   };
 
   return (
@@ -19,10 +33,15 @@ const Layout: React.FC<LayoutProps> = ({ children, title, hasBackButton }) => {
       <header className='relative flex items-center justify-around h-16'>
         {hasBackButton && (
           <button className='absolute left-6' onClick={onBackClick}>
-            <HiOutlineChevronLeft size={24} />
+            <HiOutlineChevronLeft size={HEADER_ICON_SIZE} />
           </button>
         )}
         <span className='font-bold'>{title}</span>
+        {!!token && (
+          <button className='absolute right-6' onClick={onLogoutClick}>
+            <HiArrowRightOnRectangle size={HEADER_ICON_SIZE} />
+          </button>
+        )}
       </header>
       <main className='px-6 mt-12 h-main'>{children}</main>
     </div>

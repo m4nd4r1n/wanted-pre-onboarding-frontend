516aaf0c3ec74a2caf6977e0a92e97786973e035
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-06-10 18:33:24 +0900

    feat: 이메일, 비밀번호 검증 추가

--- src/components/auth/Form.tsx
+++ src/components/auth/Form.tsx
@@ -4,6 +4,7 @@ import Input from './Input';
 import { AUTH_TYPE_MAP } from '@/constants/auth';
 import { PATHNAMES } from '@/constants/pathnames';
 import { TEST_IDS } from '@/constants/test-ids';
+import useValidate from '@/hooks/useValidate';
 import type { AuthPathnames } from '@/types/auth';
 
 interface FormProps {
@@ -27,21 +28,31 @@ const PasswordInputProps = {
 };
 
 const Form: React.FC<FormProps> = ({ type }) => {
+  const [isEmailValid, onEmailChange] = useValidate(/@/);
+  const [isPasswordValid, onPasswordChange] = useValidate(/.{8,}/);
+
   const isSignUp = type === SIGN_UP;
 
   return (
     <form className='flex flex-col gap-8'>
       <div className='relative'>
-        <Input testid={EMAIL_INPUT} {...EmailInputProps} />
+        <Input testid={EMAIL_INPUT} onChange={onEmailChange} {...EmailInputProps} />
+        {!isEmailValid && (
+          <span className='absolute mt-1 text-xs text-red-500'>@를 포함해 주세요.</span>
+        )}
       </div>
       <div className='relative'>
-        <Input testid={PASSWORD_INPUT} {...PasswordInputProps} />
+        <Input testid={PASSWORD_INPUT} onChange={onPasswordChange} {...PasswordInputProps} />
+        {!isPasswordValid && (
+          <span className='absolute mt-1 text-xs text-red-500'>최소 8글자 이상 입력해 주세요.</span>
+        )}
       </div>
       <Button
         type='submit'
         primary
         size='large'
         testid={isSignUp ? SIGNUP_BUTTON : SIGNIN_BUTTON}
+        disabled={!isEmailValid || !isPasswordValid}
       >
         {AUTH_TYPE_MAP[type]}
       </Button>
